3366b9b15d65bfccbae4a97d15eaf53f1c818472
Akanenosketch <akanenosketch@gmail.com>
2023-06-01 17:13:33 +0200

    deleted env and added some more shit

--- main.py
+++ main.py
@@ -1,19 +1,4 @@
-import discord
-import os
+import bot
 
-client = discord.Client()
-
-@client.event
-async def on_ready():
-  print('We have logged in as {0.user}'.format(client))
-
-
-@client.event
-async def on_message(message):
-  if message.author == client.user:
-    return 
-  if message.content('tu madre'):
-    await message.channel.send('La tuya por si acaso')
-
-
-client.run(os.getenv('token'))
+if __name__ == '__main__':
+    bot.run_discord_bot()
---
+++ responses.py
@@ -0,0 +1,7 @@
+def handle_Response(message) -> str:
+    p_message = message.lower()
+    if p_message == 'Tu madre':
+        return 'La tuya por si acaso'
+    
+    if p_message == 'fuck you':
+        return 'when, where and how hard'
