744243188bd5287461d1e2eb9c49a079409f0d44
Akanenosketch <akanenosketch@gmail.com>
2023-06-01 19:51:17 +0200

    new commands

--- bot.py
+++ bot.py
@@ -2,12 +2,12 @@ import discord
 import responses
 
 
-async def send_message(message, user_message, is_private):
+async def send_message(message, user_message, username, is_private):
   try:
-    response = responses.handle_Response(user_message)
+    response = responses.handle_Response(user_message, username)
     await message.author.send(response) if is_private else await message.channel.send(response)
   except Exception as e:
-    print(e)
+    print()
 
 def run_discord_bot():
   TOKEN = 'ODY0NTY1OTY2MTIwNjgxNTAz.GhrwMt.Th9HxRg_de168YelTbTQEH7RwWNZY7f6c9BJyY'
@@ -26,13 +26,14 @@ def run_discord_bot():
     
     username = str(message.author)
     user_message = str(message.content)
+    ##old_message = 
     channel = str(message.channel)
     
     if user_message[0] =='?':
       user_message = user_message[1:]
-      await send_message(message,user_message, is_private=True)
+      await send_message(message,user_message, username, is_private=True)
     else:
-      await send_message(message,user_message, is_private=False)
+      await send_message(message,user_message, username, is_private=False)
 
   client.run(TOKEN)
 
--- responses.py
+++ responses.py
@@ -1,7 +1,36 @@
-def handle_Response(message) -> str:
+import random 
+
+def handle_Response(message, username) -> str:
     p_message = message.lower()
     if p_message == 'tu madre':
         return 'La tuya por si acaso'
     
     if p_message == 'fuck you':
-        return 'when, where and how hard'
+        return 'when, where and how hard '+ username + ' ~'
+    
+    if p_message == 'sleep':
+        return 'what\'s sleep, is that edible?'
+    
+    if p_message == 'notas al':
+        return 'Stop trolling you fucking dingbat'
+    
+    if p_message == 'Fabi':
+        return 'Stop...changing...SHIT YOU FUCKING DICKHEAD'
+    
+    if p_message == 'future':
+        rnd = random. randrange(6)
+        msg = ' '
+        if rnd == 0:
+            msg = 'You have a "future" I guess'
+        if rnd == 1:
+            msg = 'A bright future awaits for you'
+        if rnd == 2:
+            msg = 'Your future seems good, don\'t sleep it up'
+        if rnd == 3:
+            msg = 'You\'re useless my guy'
+        if rnd == 4:
+            msg = 'You might need a sugar daddy/mommy to survive'
+        if rnd == 5:
+            msg = 'Are you sure you don\'t want to jump out or something?'
+        return msg
+
